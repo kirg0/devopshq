1f5aa29b201a6ef2828f2d67753b9a7ff0b18204
kirg0 <ipkir08@gmail.com>
2021-11-06 12:14:35 +0400

    add drone ci

--- get_issues.py
+++ get_issues.py
@@ -3,8 +3,9 @@ import os
 from pprint import pprint
 import json
 
+'''Закоментированные строки позволяют авторизироватся на GitHub, необходимо получить и добавить token. Иначе количество обращений ограничено'''
 
-token = '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'
+'''token = '''''
 org = "devopshq"
 repo = ''
 get_repo_query_url = f"https://api.github.com/orgs/{org}/repos"
@@ -12,15 +13,20 @@ params = {
     "state": "all",
 }
 i=0
-headers = {'Authorization': f'token {token}'}
-r = requests.get(get_repo_query_url, headers=headers)
-output = r.json()
-while i < len(output):
-    repo = output[i]['name']
-    print(repo)
-    get_issues_query_url = f"https://api.github.com/repos/{org}/{repo}/issues"
-    rq = requests.get(get_issues_query_url, headers=headers, params=params)
-    out = len(rq.json())
-    print('#'+str(len(rq.json())))
-    print()
-    i+=1
+'''headers = {'Authorization': f'token {token}'}'''
+try:
+    '''r = requests.get(get_repo_query_url, headers=headers)'''
+    r = requests.get(get_repo_query_url)
+    output = r.json()
+    while i < len(output):
+        repo = output[i]['name']
+        print(repo)
+        get_issues_query_url = f"https://api.github.com/repos/{org}/{repo}/issues"
+        '''rq = requests.get(get_issues_query_url, headers=headers, params=params)'''
+        rq = requests.get(get_issues_query_url, params=params)
+        out = len(rq.json())
+        print('#'+str(len(rq.json())))
+        print()
+        i+=1
+except Exception:
+    print('Ошибка подключения к репозиторию')
